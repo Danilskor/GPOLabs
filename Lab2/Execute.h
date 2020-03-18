7f7baf893ca6f89910e8d813e3e21f562b659f9d
danil <danilskor2000@mail.ru>
2020-03-18 17:05:30 +0700

    fix(Execute.h, Execute.cpp): исправлено местоположение файлов

---
+++ Lab2/Execute.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+// TODO: если подключает только вторую лабу, тогда это должно быть в папке второй лабы
+#include "Circle.h"
+#include "Enum.h"
+#include "Exception.h"
+#include "Flight.h"
+#include "Movie.h"
+#include "Rectangle.h"
+#include "DynamicStructure.h"
+#include "FunctionStructure.h"
+#include "Structure.h"
+#include "Time.h"
+
+void Lab2();
