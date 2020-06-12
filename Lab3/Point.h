afbeea2fc93b8424ad5d0e68bac9310a0f847815
danil <danilskor2000@mail.ru>
2020-06-13 02:20:16 +0700

    feature(Point): поля класса стали immutible

--- Lab3/Point.h
+++ Lab3/Point.h
@@ -5,12 +5,11 @@ class Point
 private:
 	double _x;
 	double _y;
+	void SetX(double x);
+	void SetY(double y);
 
 public:
 	Point(double x, double y);
-	
-	void SetX(double x);
-	void SetY(double y);
 
 	double GetX();
 	double GetY();
